c85868882de941a56dd3f344ba311e5dffcefd39
Henry Chen <46900345+henry256@users.noreply.github.com>
2019-04-08 01:38:21 -0400

    Add files via upload

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by user150412 on 4/7/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots{
+    var spotArray = [Spot]()
+}
